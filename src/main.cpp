0238be16f56d5cc955fa766599258025c99f9b27
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-24 08:31:43 +0200

    Refactor CreatorLayer to add map-packs button

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CreatorLayer.hpp>
+#include <Geode/binding/CCMenuItemSpriteExtra.hpp>
 
 using namespace geode::prelude;
 
-#include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/binding/CCMenuItemSpriteExtra.hpp>
 class $modify(CreatorLayer) {
 
 	bool init() {
@@ -12,34 +12,26 @@ class $modify(CreatorLayer) {
 			return false;
 		};
 
-		// Get the menu.
 		auto menu = this->getChildByID("creator-buttons-menu");
 
-		// Get the correct scale for the resolution.
-		//auto scaleToUse = getChildOfType<CCSprite>(getChildOfType<CCMenuItemSpriteExtra>(menu, 0), 0)->getScale();
+		auto sprmappacks = CCSprite::create("featured.png"_spr);
 
-		// Create the sprites for the buttons.
-		auto sprmap = CCSprite::createWithSpriteFrameName("GJ_mapBtn_001.png");
-		auto sprvers = CCSprite::createWithSpriteFrameName("GJ_versusBtn_001.png");
-
-		// Create a map to associate an ID with its new sprite.
 		std::map<std::string, CCSprite*> idsToBtns = {
-			{ "map-button", sprmap },
-			{ "versus-button", sprvers }
+			{ "map-packs-button", sprmappacks }
 		};
 		
-		// Iterate on the map to change the sprite of the buttons.
 		for (auto pair = idsToBtns.begin(); pair != idsToBtns.end(); pair++) {
 			auto id = pair->first.c_str();
-			auto superExpertLoaded = ((strcmp("versus-button", id) == 0) && (Loader::get()->isModLoaded("xanii.super_expert")));
+			auto superExpertLoaded = ((strcmp("map-packs-button", id) == 0) && (Loader::get()->isModLoaded("xanii.super_expert")));
 
-			if (!menu->getChildByID(id)) continue; // Avoid crash if child not found.
+			if (!menu->getChildByID(id)) continue;
+			if (!pair->second) continue;
 
 			auto btn = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID(id));
 			auto contentSize = btn->getContentSize();
 			auto existingSprite = btn->getChildByType<CCSprite*>(0);
 
-			if (!existingSprite) continue; // Better safe than sorry.
+			if (!existingSprite) continue;
 
 			auto scale = existingSprite->getScale();
 			pair->second->setScale(scale);
