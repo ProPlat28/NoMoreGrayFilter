fca21760951779907fd1ab4cda5a65ad7f1be24c
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-24 13:06:46 +0200

    Refactor CreatorLayer and LevelBrowserLayer initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,51 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/binding/CCMenuItemSpriteExtra.hpp>
 #include <Geode/modify/LevelBrowserLayer.hpp>
 #include <Geode/binding/GJSearchObject.hpp>
 #include <Geode/binding/LevelBrowserLayer.hpp>
+#include <Geode/binding/CCMenuItemSpriteExtra.hpp>
 
 using namespace geode::prelude;
 
 class $modify(CreatorLayer) {
-    void onMapPacks(CCObject*) {
+    bool init() {
+        if (!CreatorLayer::init()) return false;
+
+        auto menu = this->getChildByID("creator-buttons-menu");
+        if (!menu) return true;
+
+        auto sprmappacks = CCSprite::createWithSpriteFrameName("HallOfFameButton.png"_spr);
+
+        std::map<std::string, CCSprite*> idsToBtns = {
+            { "map-packs-button", sprmappacks },
+        };
+
+        for (auto& pair : idsToBtns) {
+            auto id = pair.first.c_str();
+            auto superExpertLoaded = (strcmp("versus-button", id) == 0) && Loader::get()->isModLoaded("xanii.super_expert");
+
+            if (!menu->getChildByID(id)) continue;
+
+            auto btn = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID(id));
+            if (!btn) continue;
+
+            auto contentSize = btn->getContentSize();
+            auto existingSprite = btn->getChildByType<CCSprite*>(0);
+            if (!existingSprite) continue;
+
+            pair.second->setScale(existingSprite->getScale());
+
+            if (!superExpertLoaded) {
+                btn->setNormalImage(pair.second);
+            }
+
+            btn->setContentSize(contentSize);
+        }
+
+        return true;
+    }
+
+    void onMapPacks(CCObject* target) {
         auto search = GJSearchObject::create(SearchType::HallOfFame);
         CCDirector::get()->pushScene(CCTransitionFade::create(0.5f, LevelBrowserLayer::scene(search)));
     }
@@ -17,8 +54,12 @@ class $modify(CreatorLayer) {
 class $modify(LevelBrowserLayer) {
     bool init(GJSearchObject* search) {
         if (!LevelBrowserLayer::init(search)) return false;
+
         if (search && search->m_searchType == SearchType::HallOfFame) {
-            if (auto label = this->getChildByType<CCLabelBMFont>(0)) label->setVisible(false);
+            if (auto label = this->getChildByType<CCLabelBMFont>(0)) {
+                label->setVisible(false);
+            }
+
             auto title = CCSprite::create("HallTitle.png");
             if (title) {
                 auto winSize = CCDirector::get()->getWinSize();
@@ -26,48 +67,7 @@ class $modify(LevelBrowserLayer) {
                 this->addChild(title, 10);
             }
         }
+
         return true;
     }
-};
-
-	bool init() {
-
-		if (!CreatorLayer::init()) {
-			return false;
-		};
-
-		auto menu = this->getChildByID("creator-buttons-menu");
-
-		auto sprmappacks = CCSprite::create("featured.png"_spr);
-
-		std::map<std::string, CCSprite*> idsToBtns = {
-			{ "map-packs-button", sprmappacks }
-		};
-		
-		for (auto pair = idsToBtns.begin(); pair != idsToBtns.end(); pair++) {
-			auto id = pair->first.c_str();
-			auto superExpertLoaded = ((strcmp("map-packs-button", id) == 0) && (Loader::get()->isModLoaded("xanii.super_expert")));
-
-			if (!menu->getChildByID(id)) continue;
-			if (!pair->second) continue;
-
-			auto btn = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID(id));
-			auto contentSize = btn->getContentSize();
-			auto existingSprite = btn->getChildByType<CCSprite*>(0);
-
-			if (!existingSprite) continue;
-
-			auto scale = existingSprite->getScale();
-			pair->second->setScale(scale);
-
-			if (!superExpertLoaded) {
-				btn->setNormalImage(pair->second);
-			};
-
-			btn->setContentSize(contentSize);
-
-		};
-
-		return true;
-	};
 };
